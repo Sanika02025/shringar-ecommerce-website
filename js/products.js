a313420410c785baffa99711212a3b660714041a
Sanika02025 <sanikagunjal2506@gmail.com>
2026-06-01 23:01:27 +0530

    home - wedding,heritage.. added

--- js/products.js
+++ js/products.js
@@ -1,35 +1,123 @@
 const products = [
 
 {
-id:1,
-name:"Royal Banarasi Silk Saree",
-price:4999,
-category:"Saree",
-image:"images/products/banarasi.jpg"
+    id:1,
+    name:"Paithani Silk Saree",
+    category:"Saree",
+    heritage:"Maharashtrian",
+    occasion:"Wedding",
+    price:5999,
+    image:"images/pai.jpg"
 },
 
 {
-id:2,
-name:"Temple Jewellery Set",
-price:2999,
-category:"Jewellery",
-image:"images/products/temple.jpg"
+    id:2,
+    name:"Nauvari Saree",
+    category:"Saree",
+    heritage:"Maharashtrian",
+    occasion:"Festival",
+    price:4499,
+    image:"images/nav.jpg"
 },
 
 {
-id:3,
-name:"Designer Anarkali",
-price:3499,
-category:"Ethnic Wear",
-image:"images/products/anarkali.jpg"
+    id:3,
+    name:"Kanjivaram Silk Saree",
+    category:"Saree",
+    heritage:"South Indian",
+    occasion:"Reception",
+    price:7499,
+    image:"images/kanji.jpg"
 },
 
 {
-id:4,
-name:"Paithani Saree",
-price:5999,
-category:"Saree",
-image:"images/products/paithani.jpg"
+    id:4,
+    name:"Temple Jewellery Set",
+    category:"Jewellery",
+    heritage:"South Indian",
+    occasion:"Wedding",
+    price:2999,
+    image:"images/jewel.jpg"
+},
+
+{
+    id:5,
+    name:"Bandhani Saree",
+    category:"Saree",
+    heritage:"Gujarati",
+    occasion:"Mehendi",
+    price:3999,
+    image:"images/band.jpg"
+},
+
+{
+    id:6,
+    name:"Mirror Work Lehenga",
+    category:"Lehenga",
+    heritage:"Gujarati",
+    occasion:"Sangeet",
+    price:8999,
+    image:"images/leha.jpg"
+},
+
+{
+    id:7,
+    name:"Phulkari Dupatta",
+    category:"Accessories",
+    heritage:"Punjabi",
+    occasion:"Festival",
+    price:1999,
+    image:"images/phul.jpg"
+},
+
+{
+    id:8,
+    name:"Bridal Lehenga",
+    category:"Lehenga",
+    heritage:"North Indian",
+    occasion:"Bridal",
+    price:14999,
+    image:"images/brida.jpg"
+},
+
+{
+    id:9,
+    name:"Kasavu Saree",
+    category:"Saree",
+    heritage:"Kerala",
+    occasion:"Temple",
+    price:4999,
+    image:"images/kasavu.jpg"
+},
+
+{
+    id:10,
+    name:"Mekhela Chador",
+    category:"Saree",
+    heritage:"Assamese",
+    occasion:"Traditional",
+    price:5499,
+    image:"images/chador.jpg"
+},
+
+{
+    id:11,
+    name:"Designer Kurti",
+    category:"Kurti",
+    heritage:"Modern",
+    occasion:"Casual",
+    price:1499,
+    image:"images/anar.jpg"
+},
+
+{
+    id:12,
+    name:"Wedding Blouse",
+    category:"Blouse",
+    heritage:"South Indian",
+    occasion:"Wedding",
+    price:2499,
+    image:"images/blo.jpg"
 }
 
 ];
--- js/shop.js
+++ js/shop.js
@@ -1,29 +1,28 @@
-const productContainer = document.getElementById("productContainer");
-
-function displayProducts() {
-  products.forEach((product) => {
-    productContainer.innerHTML += `
+const productContainer =
+document.getElementById("productContainer");
+function displayProducts()
+{
+productContainer.innerHTML = "";
+products.forEach(product => {
+productContainer.innerHTML += `
 
 <div class="product-card">
     <div class="product-image">
         <img src="${product.image}" alt="${product.name}">
         <span class="badge">
-            New
-        </span>
-        <span class="wishlist">
-            ♡
+            ${product.category}
         </span>
     </div>
     <div class="product-info">
         <h3>${product.name}</h3>
-        <p>${product.category}</p>
+        <p>${product.heritage}</p>
         <h4>₹${product.price}</h4>
         <button>
-            Add To Cart
+            View Product
         </button>
     </div>
 </div>
 `;
-  });
+});
 }
 displayProducts();
